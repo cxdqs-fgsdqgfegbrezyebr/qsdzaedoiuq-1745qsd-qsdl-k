8cb1cb4fa87aa10def3f51b08d650281cb166bfd
Arthur Plazanet <plazanet.art@gmail.com>
2024-04-04 12:35:18 +0200

    Remove links from the Navbar

--- components/AwesomeNavbar.vue
+++ components/AwesomeNavbar.vue
@@ -6,11 +6,6 @@
       <li><a href="/">Home 2</a></li>
       <li><a href="/">Home 3</a></li>
       <li><a href="/">Home 4</a></li>
-      <li><a href="/">Home 5</a></li>
-      <li><a href="/">Home 6</a></li>
-      <li><a href="/">Home 7</a></li>
-      <li><a href="/">Home 8</a></li>
-      <li><a href="/">Home 9</a></li>
     </ul>
   </nav>
 </template>
