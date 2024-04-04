c9932edf24e65195a5b61d99f9f57428bbc9c4e6
Arthur Plazanet <plazanet.art@gmail.com>
2024-04-04 12:21:52 +0200

    First version

---
+++ components/AwesomeNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    Navigation
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Home 2</a></li>
+      <li><a href="/">Home 3</a></li>
+      <li><a href="/">Home 4</a></li>
+      <li><a href="/">Home 5</a></li>
+      <li><a href="/">Home 6</a></li>
+      <li><a href="/">Home 7</a></li>
+      <li><a href="/">Home 8</a></li>
+      <li><a href="/">Home 9</a></li>
+    </ul>
+  </nav>
+</template>
